2660b7c085a9e8f95ef9f9ecae8be20f0a929a98
Bruno Bennotti <brunobennotti@gmail.com>
2022-12-12 12:07:51 -0300

    fix: contantes

--- aplicacao1/src/electron/constants.js
+++ aplicacao1/src/electron/constants.js
@@ -1,16 +1,6 @@
 module.exports = {
-    LISTAR_ARQUIVOS_RECENTES: 'listar-arquivos-recentes',
-    SELECIONAR_ARQUIVO_ABRIR: 'selecionar-arquivo-abrir',
-    ARQUIVOS_RECENTES_REMOVER: 'arquivos-recentes-remover',
-
-    ABRIR_BANCO_DADOS: 'abrir-banco-dados',
-    
-    SELECIONAR_ARQUIVO_SALVAR: 'selecionar-arquivo-salvar',
-    NOVO_BANCO_DADOS: 'novo-banco-dados',
 
     CLOSE_APP: 'close-app',
-
-    CARTEIRA_LISTAR: 'carteira-listar',
 
     invokeAllowListArray: [],
     sendAllowListArray: [],
